401514ab8b0f75445a2542e6aba6fdb4ac37445d
Edimar Nascimento <edimar.daros@gmail.com>
2022-09-09 15:32:19 -0300

    Feat: styled button 1

--- alura-studies/src/components/Button/index.tsx
+++ alura-studies/src/components/Button/index.tsx
@@ -1,1 +1,17 @@
-export {}
+import React from 'react';
+
+class Botao extends React.Component {
+  render() {
+    const backgroundColor = 'red';
+    const styles = {
+      backgroundColor
+    }
+    return (
+      <button style={styles}>
+        Botão
+      </button>
+    )
+  }
+}
+
+export default Botao;
